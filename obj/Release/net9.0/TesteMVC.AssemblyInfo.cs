d2b32ead8192b25927ae2a1f6cbbab46d0828ddf
Lucas Panhossi Ferreira <lucaspf517@gmail.com>
2025-12-01 19:53:00 -0300

    .

--- obj/Release/net9.0/TesteMVC.AssemblyInfo.cs
+++ obj/Release/net9.0/TesteMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TesteMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da2acbba003beb0bd727126affc03cbe27436549")]
 [assembly: System.Reflection.AssemblyProductAttribute("TesteMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TesteMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

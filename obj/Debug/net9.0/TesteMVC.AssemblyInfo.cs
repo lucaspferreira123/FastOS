1e5f290d7924b9bc6bbbf8c3b147f6b9c86c3c7f
Lucas Panhossi Ferreira Ferreira <lucaspf517@gmail.com>
2025-12-27 04:26:27 -0300

    Alterações do appsettings.json para conter o padrão de conexão utilizada ao criar o imagem do sql server no docker

--- obj/Debug/net9.0/TesteMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/TesteMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TesteMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b32ead8192b25927ae2a1f6cbbab46d0828ddf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ae1a1259e899442da21198b1ddd67d1b5d328a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TesteMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TesteMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
